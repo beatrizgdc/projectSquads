718038f96497ed67a0735676cd06e50b56f1d078
Jonnathan Vituriano <viturianojonnathan@gmail.com>
2024-10-17 10:00:34 -0300

    🎉 [init] connect do banco de dados.

--- projeto/backend/routes/route.js
+++ projeto/backend/routes/route.js
@@ -1,1 +1,10 @@
 // Definição das rotas da API
+
+const express = require('express');
+const router = express.Router();
+const { createUser } = require('../controllers/controller.js');
+
+// Rota para criar um novo usuário
+router.post('/register', createUser);
+
+module.exports = router;
